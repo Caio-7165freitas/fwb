fb0c003283ec1b9a09a82f09daf8770798f731a9
Caio-7165freitas <caio19cesar@gmail.com>
2023-07-25 19:37:13 -0300

    caio

--- database/migrations/2023_07_14_020753_create_produtos_table.php
+++ database/migrations/2023_07_14_020753_create_produtos_table.php
@@ -20,6 +20,8 @@ return new class extends Migration
             $table->float('precoUnitario');
 
             $table->float('precoVenda');
+            
+            $table->timestamps();
         });
     }
 
